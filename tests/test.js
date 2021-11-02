0c35b74b749b3cbca04cf95a0820f2fcb46e2a06
Adam Crampton <adam@adamcrampton.com>
2021-11-03 09:54:14 +1100

    Pretty much working

--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,30 @@
-const { By, Key, Builder } = require('selenium-webdriver');
+const { By, Key, Builder, WebDriver } = require('selenium-webdriver');
 require('chromedriver');
 
 async function launch() {
-    // Load webdriver and specify browser endpoint.
-    let driver = await new Builder().forBrowser("chrome").build();
+    // Set up our parameters.
     const endpoint = 'https://convo.ink';
+    const elementId = 'platform';
+    const attempts = 1;
+    
+    // Run test for specified attempt count.
+    for (let index = 1; index <= attempts; index++) {
+        const driver = await new Builder().forBrowser('chrome').build();
+        // Fetch page.
+        await driver.get(endpoint);
 
-    // Fetch page.
-    await driver.get(endpoint);
+        // Ensure we can locate target element, then scroll.
+        driver.wait(() => {
+            return driver.findElement(By.id(elementId))
+        }, 5000);
 
-    // Close off browser connection.
-    await driver.quit();
+        driver.executeScript('arguments[0].scrollIntoView(true);', driver.findElement(By.id(elementId)));
+
+        // Wait 5 seconds, then close off browser connection.
+        setTimeout(() => {
+            driver.quit();
+        }, 5000);
+    }
 }
 
 launch();
