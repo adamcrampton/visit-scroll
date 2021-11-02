6988a6daa687ca8d08356d52626c9db284f188ee
Adam Crampton <adam@adamcrampton.com>
2021-11-03 10:54:40 +1100

    Prompt for loop count

--- fire.sh
+++ fire.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Ask for loop count.
+echo Enter number of page loads
+read attempts
+
 # Run Selenium tests using specified count.
 counter=0
-attempts=3
 while [  $counter -lt $attempts ]; do
     let counter=counter+1
-    echo Running test $counter of $attempts ...
+    echo Loading page $counter of $attempts ...
     node tests/test.js
 done
-echo Tests complete.
+echo All done.
